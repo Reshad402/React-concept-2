d8ca755e26aa3dbe279179fc6575d6866683397c
Reshad402 <mdrhr402@gmail.com>
2022-10-24 19:38:43 +0600

    Dynamically

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+const products=[
+  {name:'Laptop',price:'84000'},
+  {name:'Mobile',price:'8400'},
+  {name:'Watch',price:'400'},
+
+]
 function App() {
   return (
     <div className="App">
-      <Product name="Laptop" price="80000"></Product>
-      <Product name="Motor" price="15000"></Product>
+      {/* <Product name="Laptop" price="80000"></Product> */}
+      {
+        products.map(product =><Product name={product.name} price={product.price}></Product>)
+      }
+      {/* <Product name="Motor" price="15000"></Product> */}
     </div>
   );
 }
